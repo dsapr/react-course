f411bf6ef990c76a28f7cc02e1d57c00f0dce297
chenyi wang <785722840@qq.com>
2022-12-08 17:38:00 +0800

    course 032 Scoping Component Styles With CSS Modules

--- react-course-02/src/components/layout/MainNavigation.js
+++ react-course-02/src/components/layout/MainNavigation.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 
+import classes from './MainNavigation.module.css'
+
 function MainNavigation() {
   return (
-    <header>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
