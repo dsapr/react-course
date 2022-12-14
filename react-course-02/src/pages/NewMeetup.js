53ce3fd088751b01999e955648fd44c1b5f7b431
chenyi wang <785722840@qq.com>
2022-12-14 11:24:25 +0800

    course 041 Getting Started with Fetching Data

--- react-course-02/src/pages/NewMeetup.js
+++ react-course-02/src/pages/NewMeetup.js
@@ -1,10 +1,30 @@
-import NewMeetupForm from "../components/meetups/NewMeetupForm"
+import { useHistory } from 'react-router-dom'
 
-function NewMeetupPage () {
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
+function NewMeetupPage() {
+  const history = useHistory()
+
+  function addMeetupHandler(meetupData) {
+    // 支持 JavaScript 标准函数，可用 axios 代替
+    fetch(
+      'https://react-getting-started-3f574-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/')
+    })
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
